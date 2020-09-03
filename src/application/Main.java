4cde1f64fb6fcf6308419b372036eeb43df52af1
Kamini Kumari <kk21071998@gmail.com>
2020-09-03 14:19:33 +0530

    Title Changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
         
         Scene scene = new Scene(root);
-        stage.setTitle("Calculator");
+        stage.setTitle("Simple Calculator");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
